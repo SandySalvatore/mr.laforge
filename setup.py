3c2d3347d7c80e948d0d20a4a808481a9fd2698a
SandySalvatore <ts-lankishetty.sande@rakuten.com>
2019-08-07 10:13:49 +0530

    Version modified to 0.6.altair

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-version = '0.7'
+version = '0.6.altair'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
